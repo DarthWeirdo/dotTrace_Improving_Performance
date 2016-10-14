ef493bc33be53ce5487151910803732f4367ac5f
DarthWeirdo <atot1981@gmail.com>
2016-10-14 13:35:38 +0200

    First commit

---
+++ MassFileProcessing/FileProcessor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Windows;
+
+namespace MassFileProcessing
+{
+    internal class FileProcessor
+    {
+        public List<string> FilePaths;
+        private string[] _lines;
+        public readonly BackgroundWorker Worker;
+        private readonly ProgressUpdater _updater;
+
+
+        public FileProcessor()
+        {
+            _updater = new ProgressUpdater();
+            Worker = new BackgroundWorker {WorkerReportsProgress = true};
+            Worker.DoWork += ProcessFiles;
+        }
+
+        private void ProcessFiles(object sender, DoWorkEventArgs evts)
+        {
+            try
+            {
+
+                var reverser = new StringReverser();  
+
+                _updater.TotalFiles = FilePaths.Count;
+
+                for (var i = 0; i < FilePaths.Count; i++)
+                {
+                    
+                    _updater.CurrentFileNmb = i + 1;
+
+                    var path = FilePaths[i];
+                    _lines = File.ReadAllLines(path);
+                    
+                    for (var j = 0; j < _lines.Length; j++)
+                    {
+                        var line = _lines[j];
+                        _lines[j] = reverser.Reverse(line);
+
+                        if (j%1000 != 0) continue;
+                        var p = ((float)(j + 1) / _lines.Length) * 100;
+                        Worker.ReportProgress((int) p, _updater);
+                    }
+
+                    File.WriteAllLines(path, _lines);                    
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            
+        }
+
+    }
+}
---
+++ MassFileProcessing/StringReverser.cs
@@ -0,0 +1,17 @@
+﻿namespace MassFileProcessing
+{
+    internal class StringReverser
+    {
+        public string Reverse(string line)
+        {
+            var charArray = line.ToCharArray();
+            string stringResult = null;
+
+            for (var i = charArray.Length; i > 0; i--)
+            {
+                stringResult += charArray[i - 1];
+            }
+            return stringResult;
+        }
+    }
+}
